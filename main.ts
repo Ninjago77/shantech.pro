502ebe1405459392db379985d33f8e0d8b098773
Ninjago77 <shanvantharunmozhi@gmail.com>
2024-06-28 11:56:58 -0700

    maze algortihtm origin shift implenting

--- main.ts
+++ main.ts
@@ -11,17 +11,33 @@ enum Cell{
 const randomInt = (min, max) =>
     Math.floor(Math.random() * (max - min + 1)) + min;
 
-class Maze {
+class OriginShiftMaze {
     public matrix: Cell[][];
     public matrixHeight: number;
     public matrixWidth: number;
+    public originCellX: number;
+    public originCellY: number;
     constructor(public cellHeight: number, public cellWidth: number) {
         this.matrixHeight = cellHeight*2 - 1;
         this.matrixWidth = cellWidth*2 - 1;
+        this.originCellX = cellWidth - 1;
+        this.originCellY = cellHeight - 1;
         this.matrix = new Array(this.matrixHeight).fill(0).map(() => new Array(this.matrixWidth).fill(0).map(() => Cell.NONE));
         for (var i = 0; i < this.cellHeight; i++) {
             for (var j = 0; j < this.cellWidth; j++) {
-                this.matrix[Maze.cellCoordinateToSize(i)][Maze.cellCoordinateToSize(j)] = Cell.BLOCK;
+                this.matrix[OriginShiftMaze.cellCoordinateToSize(i)][OriginShiftMaze.cellCoordinateToSize(j)] = Cell.BLOCK;
+            }
+        }
+        for (var i = 0; i < this.cellHeight; i++) {
+            for (var j = 0; j < this.matrixWidth; j++) {
+                if (this.matrix[OriginShiftMaze.cellCoordinateToSize(i)][j] !== Cell.BLOCK) {
+                    this.matrix[OriginShiftMaze.cellCoordinateToSize(i)][j] = Cell.RIGHT;
+                }
+            }
+        }
+        for (var i = 0; i < this.matrixHeight; i++) {
+            if (this.matrix[i][this.matrixWidth-1] !== Cell.BLOCK) {
+                this.matrix[i][this.matrixWidth-1] = Cell.DOWN;
             }
         }
     }
@@ -35,7 +51,7 @@ class Maze {
 }
 
 function drawMaze(ctx:CanvasRenderingContext2D) {
-    var m = new Maze(5,5);
+    var m = new OriginShiftMaze(5,5); // new Maze(5,5);
     console.log(m);
 }
 
